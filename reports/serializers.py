cc841bf97a753bd6df19d758aac0a044164d62d7
loaif <loaif.is@!gmail.com>
2016-07-12 17:12:56 -0300

    Fixing bugs

--- reports/serializers.py
+++ reports/serializers.py
@@ -22,10 +22,7 @@ class SectionSerializer(serializers.ModelSerializer):
         model = Section
         fields = ('id', 'order', 'report', 'title', 'content', 'children')
 
-"""
-Adding the new attributes (date and incidents_id).
 
-"""
 class MapPointSerializer(serializers.ModelSerializer):
     class Meta:
         model = MapPoint
